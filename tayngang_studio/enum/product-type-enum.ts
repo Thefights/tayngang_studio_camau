e192938a9f087b9f15f49984a7289a0c51038477
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-09-18 15:09:22 +0700

    Revert "Add model and lib"
    This reverts commit 03f967592619b700a5ce19397172e98057bca0e1.

--- tayngang_studio/enum/product-type-enum.ts
+++
@@ -1,5 +0,0 @@
-export enum ProductType {
-	NOTEBOOK = 'NOTEBOOK',
-	KEYCHAIN = 'KEYCHAIN',
-	COMBO = 'COMBO',
-}
--- tayngang_studio/utills/string-util.ts
+++
@@ -1,3 +0,0 @@
-export const formatTitle = (title: string) => {
-  return `${title} | ${process.env.NEXT_PUBLIC_WEB_NAME}`;
-};
