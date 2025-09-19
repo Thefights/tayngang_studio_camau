0dbd13aea8161188e223220c8ee984de8cb501e9
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-09-20 01:24:08 +0700

    test

--- tayngangstudio_sever/tayngangstudio_sever/Controllers/WeatherForecastController.cs
+++ tayngangstudio_sever/tayngangstudio_sever/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace tayngangstudio_sever.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" , "SHelsjdakwd"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
