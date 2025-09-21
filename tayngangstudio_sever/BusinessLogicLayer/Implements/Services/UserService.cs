f535cde5092cb104a4a48284bea46b511f1a1508
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-09-21 18:28:03 +0700

    Change format of UserService

--- tayngangstudio_sever/BusinessLogicLayer/Implements/Services/UserService.cs
+++ tayngangstudio_sever/BusinessLogicLayer/Implements/Services/UserService.cs
@@ -1,16 +1,15 @@
 ﻿using DataAccessLayer.Models.UserEntities;
 using DataAccessLayer.Repository.Base;
-using static BusinessLogicLayer.Implements.Services.UserService;
 
 namespace BusinessLogicLayer.Implements.Services
 {
+    public interface IUserService
+    {
+        public Task<User> GetUserById(int id);
+    }
+
     public class UserService(IUnitOfWork _unitOfWork) : IUserService
     {
-        public interface IUserService
-        {
-            public Task<User> GetUserById(int id);
-        }
-
         public async Task<User> GetUserById(int id)
         {
             return await _unitOfWork.Repository<User>().GetByIdAsync(id);
