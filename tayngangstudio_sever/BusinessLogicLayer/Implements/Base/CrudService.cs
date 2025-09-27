3b96e2a04785b49ba5d8764ab92fbbdcda9fd62d
Shinjuuichi <141734387+shinjuuichi@users.noreply.github.com>
2025-09-27 15:58:28 +0700

    Fix base update

--- tayngangstudio_sever/BusinessLogicLayer/Implements/Base/CrudService.cs
+++ tayngangstudio_sever/BusinessLogicLayer/Implements/Base/CrudService.cs
@@ -12,7 +12,7 @@ namespace BusinessLogicLayer.Implements.Base
         public Task<GetDTO> GetByIdAsync(int id);
         public Task<IEnumerable<GetDTO>> GetAllAsync();
         public Task<CreateDTO> CreateAsync(CreateDTO dto);
-        public Task UpdateAsync(UpdateDTO dto);
+        public Task UpdateAsync(int id, UpdateDTO dto);
         public Task DeleteAsync(int id);
     }
 
@@ -84,9 +84,10 @@ namespace BusinessLogicLayer.Implements.Base
             return _mapper.Map<CreateDTO>(entity);
         }
 
-        public virtual async Task UpdateAsync(UpdateDTO dto)
+        public virtual async Task UpdateAsync(int id, UpdateDTO dto)
         {
-            var entity = _mapper.Map<T>(dto);
+            var entity = await _unitOfWork.Repository<T>().GetByIdAsync(id) ?? throw new KeyNotFoundException($"Entity with ID {id} not found.");
+            _mapper.Map(dto, entity);
 
             // Check if entity is ImageEntity and handle image upload automatically
             if (entity is ImageEntity imageEntity && _imageUploadService != null)
