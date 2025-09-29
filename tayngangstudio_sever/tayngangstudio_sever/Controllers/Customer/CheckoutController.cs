67b9403bd00945db9f59051403aa349f9832d37a
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-09-29 19:59:50 +0700

    ma met qua

--- tayngangstudio_sever/tayngangstudio_sever/Controllers/Customer/CheckoutController.cs
+++ tayngangstudio_sever/tayngangstudio_sever/Controllers/Customer/CheckoutController.cs
@@ -1,16 +1,14 @@
-﻿using BusinessLogicLayer.Attributes;
-using BusinessLogicLayer.DTO;
+﻿using BusinessLogicLayer.DTO;
 using BusinessLogicLayer.Implements.Services;
 using DataAccessLayer.Enums;
 using Microsoft.AspNetCore.Mvc;
-using Net.payOS;
 using Net.payOS.Types;
 
 namespace tayngangstudio_sever.Controllers.Customer
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CheckoutController(ICheckoutService _checkoutService, PayOS _payOS) : ControllerBase
+    public class CheckoutController(ICheckoutService _checkoutService) : ControllerBase
     {
         [HttpPost]
         public async Task<IActionResult> Checkout([FromBody] CheckoutRequestDTO dto)
@@ -21,20 +19,14 @@ namespace tayngangstudio_sever.Controllers.Customer
         }
 
         [HttpPost("payos/webhook")]
-        [AllowAnonymous]
-        [Consumes("application/json")]
-        public async Task<IActionResult> PayOSWebhook([FromBody] WebhookType? body)
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<IActionResult> PayOSWebhook([FromBody] WebhookData? payload)
         {
-            // 1) Xác minh chữ ký + bóc dữ liệu theo đúng SDK
-            WebhookData data = _payOS.verifyPaymentWebhookData(body); // trả về WebhookData đã verify
-
-            // 2) Xử lý nghiệp vụ
-            if (data.code == "00")
+            if (payload.code == "00")
             {
-                await _checkoutService.UpdateOrderStatus(data.orderCode);
+                await _checkoutService.UpdateOrderStatus(payload.orderCode);
             }
 
-            // 3) Trả 200 cho PayOS
             return Ok();
         }
     }
