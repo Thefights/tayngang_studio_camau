4fddb2c789847b674473fe0b6ab63d59d8f65578
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-09-29 21:56:38 +0700

    test 1

--- tayngangstudio_sever/tayngangstudio_sever/Controllers/Customer/CheckoutController.cs
+++ tayngangstudio_sever/tayngangstudio_sever/Controllers/Customer/CheckoutController.cs
@@ -2,13 +2,15 @@
 using BusinessLogicLayer.Implements.Services;
 using DataAccessLayer.Enums;
 using Microsoft.AspNetCore.Mvc;
+using Net.payOS;
 using Net.payOS.Types;
+using NetCoreDemo.Types;
 
 namespace tayngangstudio_sever.Controllers.Customer
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CheckoutController(ICheckoutService _checkoutService, ILogger<CheckoutController> _logger) : ControllerBase
+    public class CheckoutController(ICheckoutService _checkoutService, ILogger<CheckoutController> _logger, PayOS _payOS) : ControllerBase
     {
         [HttpPost]
         public async Task<IActionResult> Checkout([FromBody] CheckoutRequestDTO dto)
@@ -35,6 +37,13 @@ namespace tayngangstudio_sever.Controllers.Customer
                 await _checkoutService.UpdateOrderStatus(payload.orderCode);
             }
 
+            return Ok();
+        }
+
+        [HttpPost("confirm-webhook")]
+        public async Task<IActionResult> ConfirmWebhook(ConfirmWebhook body)
+        {
+            await _payOS.confirmWebhook(body.webhook_url);
             return Ok();
         }
     }
