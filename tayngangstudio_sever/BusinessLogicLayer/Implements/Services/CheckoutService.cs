d6c75f31ca8e46817d4b9b58498ad25f00f84f35
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-09-29 20:26:57 +0700

    Do db bi ngu chu k phai toi sai

--- tayngangstudio_sever/BusinessLogicLayer/Implements/Services/CheckoutService.cs
+++ tayngangstudio_sever/BusinessLogicLayer/Implements/Services/CheckoutService.cs
@@ -54,30 +54,39 @@ namespace BusinessLogicLayer.Implements.Services
 
         private async Task<string> CreatePaymentLink(int orderId)
         {
-            var order = await GetOrderById(orderId);
-            string description = "Order Payment";
-
-            List<ItemData> items = new List<ItemData>();
-
-            foreach (var detail in order.OrderDetails)
+            try
             {
-                var product = await GetProductById(detail.ProductId);
-                ItemData item = new ItemData(
-                     product.Name,
-                     detail.Quantity,
-                     (int)detail.UnitPrice
-     );
-                items.Add(item);
+                var order = await GetOrderById(orderId);
+                int orderCode = int.Parse(DateTimeOffset.Now.ToString("ffffff"));
+                string description = "Order Payment";
+
+                List<ItemData> items = new List<ItemData>();
+
+                foreach (var detail in order.OrderDetails)
+                {
+                    var product = await GetProductById(detail.ProductId);
+                    ItemData item = new ItemData(
+                         product.Name,
+                         detail.Quantity,
+                         (int)detail.UnitPrice
+         );
+                    items.Add(item);
+                }
+                var totalAmount = items.Sum(i => i.quantity * i.price);
+                var cancelUrl = "https://fptsoftware.com/";
+                var returnUrl = "http://localhost:3000/checkout/success/";
+
+                PaymentData paymentData = new PaymentData(orderCode, totalAmount, description, items, cancelUrl, returnUrl);
+
+                CreatePaymentResult createPayment = await _payOS.createPaymentLink(paymentData);
+
+                return createPayment.checkoutUrl;
             }
-            var totalAmount = items.Sum(i => i.quantity * i.price);
-            var cancelUrl = "https://fptsoftware.com/";
-            var returnUrl = "http://localhost:3000/checkout/success/";
+            catch (Exception ex)
+            {
 
-            PaymentData paymentData = new PaymentData(orderId, totalAmount, description, items, cancelUrl, returnUrl);
-
-            CreatePaymentResult createPayment = await _payOS.createPaymentLink(paymentData);
-
-            return createPayment.checkoutUrl;
+                throw new Exception("", ex);
+            }
         }
 
         public async Task ConfirmWebhook(string webhookUrl)
